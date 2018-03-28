934d85b18a9d6f154d775209590ff790e288c7ce
Alex Verbruggen <verbruggenalex@gmail.com>
2018-03-28 01:56:27 -0700

    Try complete tactic

--- src/Client.php
+++ src/Client.php
@@ -203,7 +203,7 @@ namespace EC\Utils\Backtrac {
          */
         public function waitForResults($id, $timeout = 10)
         {
-            while (strpos($this->getResult($id)->result->message, 'progress') !== false) {
+            while (strpos($this->getResult($id)->result->message, 'complete') === false) {
                 sleep($timeout);
             }
             return $this->getResult($id);
