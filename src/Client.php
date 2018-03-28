8cc07285dba9338d98aeef14829c94b107ab6df7
verbruggenalex <verbruggenalex@gmail.com>
2018-03-28 12:59:00 +0200

    Update Client.php

--- src/Client.php
+++ src/Client.php
@@ -124,7 +124,7 @@ namespace EC\Utils\Backtrac {
         public function checkResponse(\RestClient $client)
         {
             if (empty($client->response)) {
-                throw new \Exception('Empty response from API');
+                return false;
             }
             $response = json_decode($client->response);
             if (empty($response) || $response->status !== "success") {
@@ -203,7 +203,7 @@ namespace EC\Utils\Backtrac {
          */
         public function waitForResults($id, $timeout = 10)
         {
-            while (strpos($this->getResult($id)->result->message, 'progress') !== false) {
+            while ($result = $this->getResult($id) && strpos($result->result->message, 'progress') !== false) {
                 sleep($timeout);
             }
             return $this->getResult($id);
